1d65f6aef19d6dc1f2a5f2f8290d927505dd7e32
arosa822 <h.rosa822@gmail.com>
2019-03-28 17:20:45 -0600

    fix timestamps for expected schedule

--- 2_Device_1_Plot.py
+++ 2_Device_1_Plot.py
@@ -2,6 +2,7 @@
 import sys
 import re
 import datetime
+from datetime import date, timedelta
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -18,13 +19,12 @@ LOG2 = ('./data/device_2.log')
 #LOG=sys.argv[1] 
 
 # predefined schedule
-SCHED = {'8:00':'Lower','10:30':'Raise',
-        '10:35':'Lower','12:30':'Raise',
-        '12:35':'Lower','13:30':'Raise',
-        '13:35':'Lower','14:30':'Raise',
-        '14:35':'Lower','16:30':'Raise',
-        '16:35':'Lower','19:30':'Raise',
-        '19:35':'Lower','10:00':'Raise'}
+SCHED = {'8:00':0,'10:30':100,
+        '10:35':0,'12:30':100,
+        '12:35':0,'13:30':100,
+        '13:35':0,'14:30':100,
+        '14:35':0,'16:30':100,
+        '16:35':0,'19:30':100,}
 
 
 # generator for grabbing the days within timespan of log
@@ -32,7 +32,44 @@ def daterange(start_date,end_date):
         for n in range(int((end_date-start_date).days)):
                     yield start_date + timedelta(n)
 
-                    
+# depending on the span of time create a schedule based on the data above
+def generateSchedule(listofDateTime):
+    span = [min(listofDateTime),max(listofDateTime)]
+    t=[]
+    s=[]
+    # format the schedules to time
+    for key in SCHED:
+        t.append(key)
+        s.append(SCHED[key])
+    #t=[datetime.strptime(i,'%H:%M').time() for i in t]
+
+    d = {}
+    for single_date in daterange(span[0],span[1]+timedelta(days=1)):
+        d[single_date] = [t,s]
+
+    return d
+
+def expandToList(dictionary):
+    '''This module expands the dictionary of schedules into a list, dates need to be duplicated to fill t
+    '''
+    _date = []
+    _time = []
+    _state =[]
+    for k in dictionary:
+        for i in dictionary[k][0]:
+            _time.append(i)
+        for i in dictionary[k][1]:
+            _state.append(i)
+        for i in range(0,len(dictionary[k][1])):
+            _date.append(k)
+    #HACK: combine datetime and day
+    n = 0
+    for i in _date:
+        _date[n] = datetime.datetime.combine(_date[n].date(),datetime.datetime.strptime(_time[n],'%H:%M').time())
+        n += 1
+    df=pd.DataFrame({'Datetime':_date,'Action':_state})
+    return df
+
 def filterLog(string):
    
     try:
@@ -103,43 +140,61 @@ def main():
 
     print('sorting data for {}'.format(LOG1))
     print(len(t_obj_1),len(s_1))
+    schedule = generateSchedule(t_obj_1)
 
+    d = expandToList(schedule)
+    print(d['Datetime'],d['Action'])
+            
     return
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
-#app.layout = html.Div(children=[
-#html.H1(children='Schedule Testing '),
-#
-#html.Div(children=''),
-#
-#    dcc.Graph(
-#        id='example-graph',
-#
-#        figure={
-#            'data': [
-#                {'x': t_obj_1, 'y': s_1, 'type': 'line', 'name': sys.argv[1]},
-#                {'x': t_obj_2, 'y': s_2, 'type': 'line', 'name': sys.argv[2]}
-#
-#            ],
-#
-#
-#            'layout': {
-#                'title': 'observed:' + LOG
-#
-#            }
-#        }
-#    ),
-#    # placeholder for data https://dash.plot.ly/datatable see for reference
-#    dash_table.DataTable(
-#        id='table',
-#        columns=[{"name": i, "id": i} for i in df_1.columns],
-#        data=df_1.to_dict("rows"),
-#
-#                    )
-#])
+
+[s_1,t_obj_1,df_1]=findEdges(LOG1)
+
+[s_2,t_obj_2,df_2]=findEdges(LOG2)
+
+print('sorting data for {}'.format(LOG1))
+print(len(t_obj_1),len(s_1))
+schedule = generateSchedule(t_obj_1)
+
+d = expandToList(schedule)
+##########################################################################
+# start of dash visualizations
+##########################################################################
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+html.H1(children='Data Vizzzz..  '),
+
+html.Div(children=''),
+
+    dcc.Graph(
+        id='example-graph',
+
+        figure={
+            'data': [
+                {'x': t_obj_1, 'y': s_1, 'type': 'line', 'name': LOG1},
+                {'x': d['Datetime'], 'y': d['Action'], 'type': 'line', 'name':'Expected'}
+
+            ],
+
+
+            'layout': {
+                'title': 'simulated + ' + LOG1
+
+            }
+        }
+    ),
+    # placeholder for data https://dash.plot.ly/datatable see for reference
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df_1.columns],
+        data=df_1.to_dict("rows"),
+
+                    )
+])
 
 if __name__ == '__main__':
+    #app.run_server(debug=True,host='0.0.0.0', port = 8080)
     main()
-   # app.run_server(debug=True,host='0.0.0.0', port = 8080)
