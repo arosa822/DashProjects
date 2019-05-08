cbcbc24ad2ea4ca375365a7299464575d8cc1b5b
arosa822 <h.rosa822@gmail.com>
2019-05-08 14:01:05 -0600

    implimentation of button to update graph

--- buttons/example.py
+++ buttons/example.py
@@ -3,16 +3,31 @@ import dash_html_components as html
 import dash_core_components as dcc
 import time
 
+# Local Modules
+from parseData import parse
+
+[x,y]=parse('sample.csv')
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+    html.H1(children='Button testing...'),
+
+    # button 1
     html.Button('Refresh', id='button'),
     html.Div(id='container-button-basic',children='submit to grab data'),
-
+    
+    # button 2
     html.Button('Export', id='button_2'),
-    html.Div(id='container-button-basic_2')
+    html.Div(id='container-button-basic_2'),
+
+#    html.Div(children=''),
+#
+#        dcc.Graph(id ='line graph',
+#            figure={'data': [ {'x': x,'y': y,'type':'line','name':'csvFile'} ],
+#                'layout': {'title': 'parsedData'}})
 ])
 
 
@@ -20,10 +35,12 @@ app.layout = html.Div([
     dash.dependencies.Output('container-button-basic', 'children'),
     [dash.dependencies.Input('button', 'n_clicks')])
 def update_output(n_clicks):
-    return 'The button has been clicked {} times'.format(
-        #value,
-        n_clicks
-    )
+    [x,y] = parse('sample.csv')
+    return  html.Div(children=''),dcc.Graph(id ='line graph',
+            figure={'data': [ {'x': x,'y': y,'type':'line','name':'csvFile'} ],
+            'layout': {'title': 'parsedData'}})
+ 
+
 @app.callback(
         dash.dependencies.Output('container-button-basic_2', 'children'),
         [dash.dependencies.Input('button_2','n_clicks_timestamp')])
@@ -31,5 +48,15 @@ def update_output_2(time_pressed):
     humanTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_pressed))
     return 'this one was pressed at: {} '.format(humanTime)
 
+#@app.callback(
+#    dash.dependencies.Output('container-button-basic', 'children'),
+#    [dash.dependencies.Input('button', 'n_clicks')])
+#
+#
+#def update_output(n_clicks):
+#    return 'The button has been clicked {} times'.format(
+#        #value,
+#        n_clicks
+#    )
 if __name__ == '__main__':
     app.run_server(debug=True)
